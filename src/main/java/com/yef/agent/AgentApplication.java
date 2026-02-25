f90bb01303040b69f034e8b8ea5a3eac3f19d2b8
17665372100 <1197722073@qq.com>
2026-02-25 19:13:14 +0800

    discard useless mysql auto bean v4

--- src/main/java/com/yef/agent/AgentApplication.java
+++ src/main/java/com/yef/agent/AgentApplication.java
@@ -6,7 +6,13 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import java.io.IOException;
 
 @EnableAsync
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+                org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration.class,
+                org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
+        }
+)
 public class AgentApplication {
 
 
