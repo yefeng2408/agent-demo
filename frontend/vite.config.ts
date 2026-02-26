462a8ef2bac9babe583dca75d4174a5892a9a87b
17665372100 <1197722073@qq.com>
2026-02-26 15:33:39 +0800

    npm run buildv2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: '../src/main/resources/static'
+    //outDir: '../src/main/resources/static'
+    outDir: 'dist'
   }
 })
