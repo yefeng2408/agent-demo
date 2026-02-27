35512496785db3a218d083ade3578adf9bf7d996
17665372100 <1197722073@qq.com>
2026-02-27 21:45:33 +0800

    2

--- src/main/java/com/yef/agent/config/Neo4jDriverConfig.java
+++ src/main/java/com/yef/agent/config/Neo4jDriverConfig.java
@@ -1,14 +1,18 @@
 package com.yef.agent.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Config;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class Neo4jDriverConfig {
+
 
     @Bean
     public Driver neo4jDriver(
@@ -16,6 +20,11 @@ public class Neo4jDriverConfig {
             @Value("${neo4j.username}") String username,
             @Value("${neo4j.password}") String password
     ) {
-        return GraphDatabase.driver(uri, AuthTokens.basic(username, password));
+        log.info("Neo4j Driver config url={}  username={}  password={}", uri,username, password);
+        Config config = Config.builder()
+                .withoutEncryption()   // 等价于 encrypted=false
+                .build();
+
+        return GraphDatabase.driver(uri, AuthTokens.basic(username, password), config);
     }
 }
